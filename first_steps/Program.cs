9087e4b53b84c0a6b45aa2b5fe5225a41c350a03
Santiago <santigs211@gmail.com>
2025-06-02 00:23:06 -0300

    initializing

---
+++ first_steps/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualBasic;
+class MainClass
+{
+    static void Main(string[] args)
+    {
+
+
+        const string greeting = "Hello, World!";
+
+
+        Console.WriteLine(greeting);
+    }
+}
